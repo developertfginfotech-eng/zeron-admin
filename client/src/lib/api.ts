a2f5cafe0c260ed9e58235e6120c5746b25721d8
Barsharani Jena <Barsharanijena555@gmail.com>
2025-09-24 14:15:44 +0530

    new api

--- client/src/lib/api.ts
+++ client/src/lib/api.ts
@@ -1,5 +1,5 @@
 const isDevelopment = window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1';
-const API_BASE_URL = isDevelopment ? 'http://localhost:5000' : 'http://13.53.177.188:5000';
+const API_BASE_URL = isDevelopment ? 'http://localhost:5000' : 'http://13.50.13.193:5000';
 
 export const API_ENDPOINTS = {
   // Authentication
